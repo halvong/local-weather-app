f791017e73286cfecac0b7a0cababd351b31d1e2
hal vong <halvong@yahoo.com>
2019-09-17 13:33:57 -0700

    Chp6. Done with Weather App 2.

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -1,9 +1,21 @@
+<<<<<<< HEAD
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+
+import { CurrentWeatherComponent } from './current-weather.component'
+import {WeatherService} from '../weather/weather.service';
+import {WeatherServiceFake} from '../weather/weather.service.fake';
+
+======
+=
 import {ComponentFixture, TestBed, async} from '@angular/core/testing';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from '../material.module';
 import {WeatherService} from '../weather/weather.service';
 import {WeatherServiceFake} from '../weather/weather.service.fake';
-import { CurrentWeatherComponent } from './current-weather.component'
+import {CurrentWeatherComponent} from './current-weather.component';
+
+>>>>>>>
+dev;
 
 describe('CurrentWeatherComponent', () => {
   let component: CurrentWeatherComponent
@@ -11,10 +23,20 @@ describe('CurrentWeatherComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [CurrentWeatherComponent],
-      providers: [{provide: WeatherService, useClass: WeatherServiceFake}],
-      imports: [MaterialModule, NoopAnimationsModule],
-    }).compileComponents()
+        declarations: [CurrentWeatherComponent],
+      < <<<<<< HEAD
+        providers: [{ provide: WeatherService, useClass: WeatherServiceFake}],
+  ======
+    =
+      providers;
+  :
+    [{provide: WeatherService, useClass: WeatherServiceFake}],
+      imports;
+  :
+    [MaterialModule, NoopAnimationsModule],
+  >>>>>>>
+    dev;
+  }).compileComponents()
   }))
 
   beforeEach(() => {
